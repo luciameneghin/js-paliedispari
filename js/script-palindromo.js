009237d289d3f373d9608c3d7bafa8aa37f4759d
luciameneghin <lucia.meneghin00@gmail.com>
2024-11-19 16:29:10 +0100

    feat : palindromo exercise works without a function

--- js/script-palindromo.js
+++ js/script-palindromo.js
@@ -12,17 +12,15 @@
 //   - return 'addWord è una parola palindroma'
 // reverse() si usa per gli array, quindi devo trasformare la parola in un array girarla fare il controllo e ricomporla (?)
 
-console.log('palindromo');
-
 const addWord = prompt('Scrivi una parola');
 console.log(addWord)
 const arrayAddWord = addWord.split('').reverse().join('');
 console.log(arrayAddWord);
 
-const palindromeWord = false;
+let palindromeWord = false;
 let message;
 
-if (arrayAddWord = addWord) {
+if (arrayAddWord === addWord) {
   palindromeWord = true;
   message = 'la parola è palindroma'
 } else {
